9bcd0d6974ecc6c4a714a0df6855988fcdf7d8a3
Chris De Leon <cdeleon.ca91@gmail.com>
2021-10-09 13:52:09 -0400

    added delete method

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -19,8 +19,20 @@ export class TodosComponent implements OnInit {
       },
       {
         content: 'Second todo',
-        completed: true,
+        completed: false,
       },
     ];
+  }
+
+  toggleDone(id: number) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    });
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
   }
 }
