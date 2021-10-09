225bb4e234219ad9b07fe4c748c6eab4b9c4a079
Chris De Leon <cdeleon.ca91@gmail.com>
2021-10-09 14:09:01 -0400

    added forms

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -9,6 +9,8 @@ import { Todo } from './../../models/Todo';
 export class TodosComponent implements OnInit {
   todos!: Todo[];
 
+  inputTodo: string = '';
+
   constructor() {}
 
   ngOnInit(): void {
@@ -34,5 +36,14 @@ export class TodosComponent implements OnInit {
 
   deleteTodo(id: number) {
     this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false,
+    });
+
+    this.inputTodo = '';
   }
 }
